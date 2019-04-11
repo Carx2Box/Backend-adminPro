8ad7c4107a7aae020db73f40ddbf7bc5d38bdc9a
Antonio Carmona <acarmonacs@gmail.com>
2019-04-11 08:26:24 +0200

    Mock in JEst

---
+++ example/appSpyReplace.test.js
@@ -0,0 +1,18 @@
+const app = require('./app');
+const math = require('./math');
+
+
+describe('Example test', () => {
+    test("calls math.add", () => {
+        const addMock = jest.spyOn(math, "add");
+
+        // override the implementation
+        addMock.mockImplementation(() => "mock");
+        expect(app.doAdd(1, 2)).toEqual("mock");
+
+        // restore the original implementation
+        addMock.mockRestore();
+        expect(app.doAdd(1, 2)).toEqual(3);
+    });
+
+});
